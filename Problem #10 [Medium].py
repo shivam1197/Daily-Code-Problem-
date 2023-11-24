35928cd6aa135afbba20fe838b8b19e92d0f769b
Shivam Dubey <shivam1197@gmail.com>
2023-11-24 13:58:38 +0530

    Adding Problem 12

--- Problem #10 [Medium].py
+++ Problem #10 [Medium].py
@@ -1,21 +1,27 @@
 """Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds."""
-#This problem was asked by Apple.
+
+# This problem was asked by Apple.
 
 import time
+
 
 def job_scheduler(f, n):
     # Convert milliseconds to seconds
     delay = n / 1000
-    
+
     # Sleep for the specified delay
     time.sleep(delay)
-    
+
     # Call the provided function
     f()
 
 # Example function to be scheduled
+
+
 def example_function():
     print("Function executed!")
 
+
 # Test the job scheduler
-job_scheduler(example_function, 5000)  # Call example_function after 5 seconds (5000 milliseconds)
+# Call example_function after 5 seconds (5000 milliseconds)
+job_scheduler(example_function, 5000)
--- Problem #12 [Hard].py
+++ Problem #12 [Hard].py
@@ -6,4 +6,35 @@ For example, if N is 4, then there are 5 unique ways:
 2, 1, 1
 1, 2, 1
 1, 1, 2
-2, """
+2, 2
+
+What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
+"""
+
+# This problem was asked by Amazon.
+
+
+def climbStairsOptimized(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 2
+
+    # Initialize an array to store the number of ways to climb the staircase with up to N steps
+    dp = [0] * (n + 1)
+
+    # Base cases
+    dp[1] = 1
+    dp[2] = 2
+
+    # Fill the dp array using the recursive formula
+    for i in range(3, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    # Return the number of ways to climb the staircase with N steps
+    return dp[n]
+
+
+# Example usage
+n = 5
+print(climbStairsOptimized(n))
